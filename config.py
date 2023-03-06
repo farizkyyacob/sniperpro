86a40f511f4f1058c1f759cbcbe9fadcaebb187e
farizkyyacob <farizky.yacob@gmail.com>
2023-03-07 00:36:45 +0700

    Initial commit

---
+++ config.py
@@ -0,0 +1,12 @@
+
+#Sniper Pro
+def api_key():
+    api_key = "pzuV9VynnWgja3R45zgFoxIEJhyGuI8L2zBH7ETmQr8Yf14KyOuLVJs4N7VUUwnA"
+    return api_key
+
+
+def api_secret():
+    api_secret = "1BZezezWBqfDEvSNx8TfJl1FYqMVqUpEmIz0J8nO2TvlO6VaysnioBiRu4GoR9pa"
+    return api_secret
+
+
